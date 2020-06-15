d32550cb78e8dffb8c2fed9f4d7fa4207d056838
Karthik-Kumar02 <skarthik.sanjeev02@gmail.com>
2020-06-15 15:14:58 +0530

    Laser-Defender

---
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
